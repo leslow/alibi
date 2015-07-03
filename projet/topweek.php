21b42065706527b227cb646e075f5ef11d11c148
xinsinaty <jordan.venant@302-013.campus.lan>
2015-07-03 17:52:27 +0200

    ajout de fichier topweek
    ajout de branche / fichier pour le bloc top de la semaine

---
+++ projet/topweek.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/topweek.css">
+</head>
+<body>
+    <div class="topweek_wrap">
+        <div class="topweek_bloc">
+            <p class="topweek_text">Désolé mais j'ai voulu changer nos destins alors je ne suis pas venu...</p>
+            <div class="topweek_metabox">
+                <a href="" class="topweek_plusone"> +1 </a>
+                <div class="topweek_like_bloc">
+                    <div class="topweek_thumb thumbs_up"></div>
+                    <p class="topweek_like_count"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
