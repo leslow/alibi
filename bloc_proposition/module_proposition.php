263a6916dcbb09c7cb2816979c59006c69ef72e2
Newforget <gitton.maxime@gmail.com>
2015-07-09 12:44:38 +0200

    Module proposition
    aside du module

---
+++ bloc_proposition/module_proposition.php
@@ -0,0 +1,25 @@
+<html lang="fr">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link href="style/style.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+    <script src="js/jquery-1.6.4.js"></script>
+</head>
+    
+<body>
+    <div id="wrap">
+        <aside>
+            <h2>Une idée d’alibi ? Postez la !</h2>
+            <div class="espace1"></div>
+            <div id="post_proposition">
+                <h3>Envie de montrez votre créativité au service de la communauté ?</h3> <h3>Envoyez nous vos meilleurs phrases d’excuse et partagez la !</h3>
+                <div class="espace1"></div>
+                <form action="index.html" method="post" name="formPost" class="form_post_proposition">
+                    <textarea placeholder="Qu'elle est votre phrase ?" id="message" name="message"></textarea><br/>
+                    <input type="submit" value="Envoyer" style="width:50%">
+                </form>
+            </div>
+        </aside>  
+    </div>
+</body>
+</html>
